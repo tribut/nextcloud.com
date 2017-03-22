bc34517b8b83dd7606ee0271aceb618f7bad5a76
Paulo Moura <expina2@hotmail.com>
2017-03-22 18:50:03 +0000

    Detect OS with javascript and show accordin tab

--- assets/js/pages/install.js
+++ assets/js/pages/install.js
@@ -1,5 +1,5 @@
-define(["jquery", "underscore", "enquire", "velocity", "velocityUI", "scrollMagic"],
-function ($, _, enquire, velocity, velocityUI, ScrollMagic) {
+define(["jquery", "underscore", "enquire", "velocity", "velocityUI", "scrollMagic", "browserSelector"],
+function ($, _, enquire, velocity, velocityUI, ScrollMagic, browserSelector) {
 	$(document).ready(function() {
     'use strict';
     var installPage = {
@@ -7,6 +7,7 @@ function ($, _, enquire, velocity, velocityUI, ScrollMagic) {
 
           this.pinFiltersBar();
           this.matchDevice();
+          this.handlerClick();
 
           enquire.register('screen and (max-width: 480px)', {
   					//match: _.bind(this.resultsBindMobile, this)
@@ -20,7 +21,7 @@ function ($, _, enquire, velocity, velocityUI, ScrollMagic) {
   			variables : {
           linksContainerSelector : ".usefull-links",
           downloadsSectionSelector: ".downloads-container",
-					bodySelector: "body",
+					htmlSelector: "html",
           menuEntrySelector: ".toggle-menu__entry",
           serverMenuSelector: ".toggle-menu__entry:nth-child(1)",
           desktopMenuSelector: ".toggle-menu__entry:nth-child(2)",
@@ -30,27 +31,43 @@ function ($, _, enquire, velocity, velocityUI, ScrollMagic) {
           mobileDownloadSectionSelector: "#mobileDownloads",
   			},
 
+        handlerClick: function() {
+          $(this.variables.menuEntrySelector).click(event, _.bind(this.toggleSection, this));
+        },
 
+        toggleSection: function(event) {
+          var elementId = $(event.currentTarget).attr("id");
 
-				matchDevice: function() {
-					var desktop = $(this.variables.bodySelector).hasClass("Macintosh" || "Linux" || "Windows"),
-              mobile = $(this.variables.bodySelector).hasClass("Iphone" || "Ipad" || "Windows Phone" || "Android");
+          if (elementId === "server") {
+            this.matchDevice(true);
+          } if (elementId === "desktop") {
+            this.matchDevice(false, true);
+          } if (elementId === "mobile") {
+            this.matchDevice(false, false, true);
+          }
+        },
 
-          if (desktop) {
+				matchDevice: function(serverClick, desktopClick, mobileClick) {
+					var server = $(this.variables.htmlSelector).hasClass("mac" || "linux" || "windows"),
+              mobile = $(this.variables.htmlSelector).hasClass("android" || "ios" || "mobile");
+
+          if (desktopClick) {
             $(this.variables.menuEntrySelector).removeClass("toggle-menu__entry--active");
             $(this.variables.desktopMenuSelector).addClass("toggle-menu__entry--active");
             $(this.variables.serverDownloadSectionSelector).removeClass("is-visible");
             $(this.variables.mobileDownloadSectionSelector).removeClass("is-visible");
             $(this.variables.desktopDownloadSectionSelector).addClass("is-visible");
             return
-          } if (mobile) {
+            
+          } if (mobile || mobileClick) {
             $(this.variables.menuEntrySelector).removeClass("toggle-menu__entry--active");
             $(this.variables.mobileMenuSelector).addClass("toggle-menu__entry--active");
             $(this.variables.serverDownloadSectionSelector).removeClass("is-visible");
             $(this.variables.desktopDownloadSectionSelector).removeClass("is-visible");
             $(this.variables.mobileDownloadSectionSelector).addClass("is-visible");
             return
-          } else {
+
+          } if (server || serverClick) {
             $(this.variables.menuEntrySelector).removeClass("toggle-menu__entry--active");
             $(this.variables.serverMenuSelector).addClass("toggle-menu__entry--active");
             $(this.variables.mobileDownloadSectionSelector).removeClass("is-visible");
